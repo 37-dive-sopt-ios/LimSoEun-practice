b8fe5f3ca81522ae107c00ecaac26bd16417a734
soeunny11 <dlathdms1206@gmail.com>
2025-11-09 20:40:02 +0900

    fix: remove nested git repository warning

---
+++ LimSoEun-practice/sopt-37th-01Seminar/sopt-37th-01Seminar/sopt-04Seminar/Environment.swift
@@ -0,0 +1,12 @@
+//
+//  Environment.swift
+//  sopt-37th-01Seminar
+//
+//  Created by 임소은 on 11/8/25.
+//
+
+import Foundation
+
+enum Environment {
+  static let baseURL: String = "http://15.164.129.239"
+}
---
+++ LimSoEun-practice/sopt-37th-01Seminar/sopt-37th-01Seminar/sopt-04Seminar/Method.swift
@@ -0,0 +1,19 @@
+//
+//  Method.swift
+//  sopt-37th-01Seminar
+//
+//  Created by 임소은 on 11/8/25.
+//
+
+///라이브러리를 활용한다면 할 필요 X..!
+
+import Foundation
+
+/// HTTP 메서드 (Moya의 Method와 동일!)
+enum Method: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case patch = "PATCH"
+    case delete = "DELETE"
+}
